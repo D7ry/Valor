05881a1e31c2ea521feb45b131960a328e20426d
Tianyun Yuan <7ianyun@berkeley.edu>
2022-10-07 21:59:05 -0700

    init

---
+++ src/bin/settings.h
@@ -0,0 +1,10 @@
+class settings
+{
+public:
+	static void read();
+	
+	/*List of settings*/
+	inline static bool bEnablePerilousAttack = true;
+	inline static bool bEnableDodgeAI = true;
+	inline static bool bEnableTimedBlock = true;
+};
